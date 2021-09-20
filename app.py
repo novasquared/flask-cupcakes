8f410a1d97ad7208b90d7f9c776f41bcc123cde3
jeanne <novasquared@yahoo.com>
2021-09-20 14:30:55 -0500

    created model for Cupcake class and added to app.py routes

--- app.py
+++ app.py
@@ -1,1 +1,34 @@
 """Flask app for Cupcakes"""
+from flask import Flask, request, flash, jsonify
+
+from models import db, connect_db, Cupcake
+
+DEFAULT_IMG_URL = "https://tinyurl.com/demo-cupcake"
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = "secret"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///cupcakes"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+connect_db(app)
+db.create_all()
+
+@app.get('/api/cupcakes')
+def list_cupcakes():
+    cupcakes = Cupcake.query.all()
+    serialized = [c.serialize() for c in cupcakes]
+
+    return  jsonify(cupcakes=serialized)
+
+@app.get('/api/cupcakes/<int:cupcake_id>')
+def get_cupcake_info(cupcake_id):
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
+    serialized = cupcake.serialize()
+    return jsonify(cupcake=serialized)
+
+# @app.post('/api/cupcakes')
+# def create_cupcake():
+#     cupcake 
--- models.py
+++ models.py
@@ -1,1 +1,55 @@
 """Models for Cupcake app."""
+from typing import Sized
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+DEFAULT_IMG_URL = "https://tinyurl.com/demo-cupcake"
+
+def connect_db(app):
+    """Connect this database to provided Flask app.
+
+    You should call this in your Flask app.
+    """
+
+    db.app = app
+    db.init_app(app)
+
+
+class Cupcake(db.Model):
+    """Cupcake"""
+
+    __tablename__ = "cupcakes"
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True)
+
+    flavor = db.Column(
+        db.Text, 
+        nullable=False)
+
+    size = db.Column(
+        db.Text,
+        nullable=False)
+
+    rating = db.Column(
+        db.Integer,
+        nullable=False)
+
+    image = db.Column(
+        db.String,
+        nullable=False, 
+        
+    def serialize(self):
+        """Serialize to dictionary."""
+
+        return {
+            "id": self.id,
+            "flavor": self.flavor,
+            "size": self.size, 
+            "rating": self.rating, 
+            "image": self.image
+        }
+
