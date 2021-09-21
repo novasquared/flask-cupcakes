ae176542db1bc4b9f6e415eb4c27ec56d40dae85
Colin Sidberry <colinsidberry@gmail.com>
2021-09-20 18:00:36 -0700

    added testing for the delete route and began testing on the patch route

--- tests.py
+++ tests.py
@@ -1,5 +1,7 @@
 from unittest import TestCase
 
+from werkzeug.wrappers import response
+
 from app import app
 from models import db, Cupcake
 
@@ -107,3 +109,74 @@ class CupcakeViewsTestCase(TestCase):
             })
 
             self.assertEqual(Cupcake.query.count(), 2)
+
+    # test wrong id = 404
+    def test_update_cupcake_id_not_there(self):
+        with app.test_client() as client:
+            url = "/api/cupcakes/9999"
+            resp = client.patch(url, json=CUPCAKE_DATA_2)
+
+            self.assertEqual(resp.status_code, 404)
+
+    # test 0 changes = stays the same
+    def test_update_cupcake_id_not_there(self):
+        with app.test_client() as client:
+            # before db variable
+            url = url = f"/api/cupcakes/{self.cupcake.id}"
+            resp = client.patch(url, json={})
+            # after db variable
+            # before = after
+            data = resp.json()
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertEqual(data,{"cupcake":{"id":self.cupcake.id,"flavor":,"size":,"rating":,"image":}})
+            # test 1 change
+            # test changing all
+
+            # test sending incorrect data input e.g. price
+
+            # url = f"/api/cupcakes/{self.cupcake.id}"
+            # resp = client.patch(url, json=CUPCAKE_DATA_2)
+
+            # cupcake = Cupcake.query.get_or_404(self.cupcake.id)
+
+
+            # self.assertEqual(resp.status_code, 200)
+
+            # data = resp.json
+
+            # # don't know what ID we'll get, make sure it's an int & normalize
+            # self.assertIsInstance(data['cupcake']['id'], int)
+            # del data['cupcake']['id'] 
+
+            # self.assertEqual(data, {
+            #     "cupcake": {
+            #         "flavor": "TestFlavor2",
+            #         "size": "TestSize2",
+            #         "rating": 10,
+            #         "image": "http://test.com/cupcake2.jpg"
+            #     }
+            # })
+
+            # self.assertEqual(Cupcake.query.count(), 2)
+
+            
+
+    def test_delete_cupcake_not_there(self):
+        with app.test_client() as client:
+            url = "/api/cupcakes/9999"
+            resp = client.delete(url)
+
+            self.assertEqual(resp.status_code, 404)
+
+
+    def test_delete_cupcake(self):
+        with app.test_client() as client:
+            url = f"/api/cupcakes/{self.cupcake.id}"
+            resp = client.delete(url)
+
+            data = resp.json  
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertEqual(data, {"deleted": self.cupcake.id})
+            self.assertEqual(Cupcake.query.count(), 0)
